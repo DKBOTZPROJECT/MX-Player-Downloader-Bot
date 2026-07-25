227bd4f904ec0e3af8e7f3a6ffeb9e5442446247
DKBOTZ <108566855+DKBOTZPROJECT@users.noreply.github.com>
2026-07-25 18:20:58 +0530

    Added MX_PLAYER_API_KEY
    By @DKBOTZPROJECT

--- Config.py
+++ Config.py
@@ -37,6 +37,8 @@ API_ID = get_int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
+MX_PLAYER_API_KEY = os.environ.get("MX_PLAYER_API_KEY", "56JPX-2YUG0-CFWWX-6JMFI")
+
 OWNER_USERNAME = validate_username(os.environ.get("OWNER_USERNAME", ""), "DKBOTZHELP")
 UPI_ID = os.environ.get("UPI_ID", "dkbotzpro@ybl")
 
